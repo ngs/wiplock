3a716c04c37b6266304848c4eba7a0c81f2e47b2
Atsushi Nagase <a@ngs.io>
2016-09-19 12:41:32 +0900

    Add switch

--- assets/src/components/Repositories/components/ListItem/index.js
+++ assets/src/components/Repositories/components/ListItem/index.js
@@ -1,10 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 import { ListGroupItem, Image, Media } from 'react-bootstrap';
+import Switch from 'react-bootstrap-switch';
+import Icon from 'react-fa';
 import './index.styl';
+import '../../../../../../node_modules/react-bootstrap-switch/dist/css/bootstrap3/react-bootstrap-switch.css';
 
 export default class ListItem extends Component {
+  constructor(props) {
+    super(props);
+    const { locked } = props;
+    this.state = { locked };
+  }
+
   render() {
     const { item, filterText } = this.props;
+    const { locked } = this.state;
     let { login } = item.owner;
     let { name } = item;
     if (filterText) {
@@ -18,6 +28,10 @@ export default class ListItem extends Component {
             <Image src={item.owner.avatar_url} />
           </Media.Left>
           <Media.Body>
+            <div className='pull-right'>
+              <Switch onText={'\uf023'} offText={'\uf09c'} value={locked} bsSize='normal'
+                onChange={(_, locked) => this.setState({ locked })} className='fa' />
+            </div>
             <span className='title'>
               <span className='owner' dangerouslySetInnerHTML={{ __html: login + ' / ' }}></span>
               <span className='name' dangerouslySetInnerHTML={{ __html: name }}></span>
@@ -31,9 +45,13 @@ export default class ListItem extends Component {
 
 ListItem.displayName = 'Organizations.ListItem';
 
+ListItem.defaultProps = {
+  locked: false
+};
+
 ListItem.propTypes = {
   filterText: React.PropTypes.instanceOf(RegExp),
-  locked: PropTypes.bool,
+  locked: PropTypes.bool.isRequired,
   item: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
