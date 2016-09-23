1fa79390b8cfdd43627d209321be087f2c6829b3
Atsushi Nagase <a@ngs.io>
2016-09-23 12:01:18 +0900

    Add blog link

--- assets/src/components/Signin/index.js
+++ assets/src/components/Signin/index.js
@@ -9,11 +9,16 @@ export default class Siginin extends Component {
       <div id='signin'>
         <div className='container text-center'>
           <h1>Wiplock</h1>
-          <Button href='/login' bsSize='large'>
-            Sign in with
-            {' '}
-            <Icon name='github' />
-          </Button>
+          <p className='signin-button'>
+            <Button href='/login' bsSize='large'>
+              Sign in with
+              {' '}
+              <Icon name='github' />
+            </Button>
+          </p>
+          <p className='whats-this'>
+            <a href='https://ngs.io/2016/09/23/wiplock/' target='_blank'>What&apos; this?</a>
+          </p>
         </div>
       </div>
     );
